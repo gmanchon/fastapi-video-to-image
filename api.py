992f1dbae49e78b051a191de876154a695034c4a
Gaetan Manchon <gmanchon@gmail.com>
2022-03-09 11:51:21 +0100

    update api to return streamed generated gif

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
 
 import aiofiles
+import imageio
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
@@ -18,10 +21,23 @@ app.add_middleware(
 @app.post("/predict")
 async def upload_file(in_file: UploadFile = File(...)):
 
+    # save video
     out_file_path = "bbb.mp4"
 
     async with aiofiles.open(out_file_path, "wb") as out_file:
         while content := await in_file.read(1024):  # async read chunk
             await out_file.write(content)  # async write chunk
 
-    return True
+    # generate gif
+    generated_gif_path = "a.gif"
+
+    images = ["a.png", "b.png", "c.png"]
+    loaded_images = [imageio.imread(f) for f in images]
+    imageio.mimsave(generated_gif_path, loaded_images)
+
+    # stream response https://fastapi.tiangolo.com/advanced/custom-response/#using-streamingresponse-with-file-like-objects
+    def iterfile():
+        with open(generated_gif_path, mode="rb") as file_like:
+            yield from file_like
+
+    return StreamingResponse(iterfile(), media_type="image/gif")
